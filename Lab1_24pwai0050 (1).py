16fdb4d2f866a0f7d06241c5c951b2c5bbae20f6
saadkhansalar <24pwai0050@uetpeshawar.edu.pk>
2026-03-03 14:58:34 +0500

    Add files via upload

---
+++ Lab1_24pwai0050 (1).py
@@ -0,0 +1,784 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "d087764b-7607-45b2-8d7c-46c8fc6b13af",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "name = \"saad khan\"\n",
+    "age = \"22\"\n",
+    "city = \"swabi\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "0b089d8a-08c8-45ce-85fa-eb0307659d8c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "saad khan\n",
+      "22\n",
+      "swabi\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(name)\n",
+    "print(age)\n",
+    "print(city)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 20,
+   "id": "aba98660-0eab-4ffd-9d44-80b1c6977b26",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "num1 = 30\n",
+    "num2 = 15"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 22,
+   "id": "465bae05-647e-4d69-9321-a561f0be43af",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Sum: 45\n",
+      "Substriction: 15\n",
+      "Multiplicatin: 450\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Sum:\", num1 + num2)\n",
+    "print(\"Substriction:\", num1 - num2)\n",
+    "print(\"Multiplicatin:\", num1 * num2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 26,
+   "id": "3ba5a690-cab7-4208-b54d-a91426a1b079",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your name saad khan salar\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "saad khan salar\n"
+     ]
+    }
+   ],
+   "source": [
+    "name =input(\"Enter your name\")\n",
+    "print(name)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 27,
+   "id": "e4faa1ca-a0da-4e63-bce7-d06aac1f2134",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your name Saad Khan Salar\n",
+      "Enter your Age 22\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Saad Khan Salar you are 22 years old\n"
+     ]
+    }
+   ],
+   "source": [
+    "name =input(\"Enter your name\")\n",
+    "Age  =input(\"Enter your Age\")\n",
+    "\n",
+    "print(\"Hello\", name , \"you are\", age , \"years old\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 30,
+   "id": "cc93d2f8-7af9-44ee-bea6-e6a62452a06b",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter firt number: 10\n",
+      "Enter second number: 5\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Sum: 15\n",
+      "Substriction: 5\n",
+      "Multiplication: 50\n"
+     ]
+    }
+   ],
+   "source": [
+    "num1 =int(input(\"Enter firt number:\"))\n",
+    "num2 =  int(input(\"Enter second number:\"))\n",
+    "\n",
+    "print(\"Sum:\", num1 + num2)\n",
+    "print(\"Substriction:\", num1 - num2)\n",
+    "print(\"Multiplication:\" , num1 * num2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 31,
+   "id": "b73a1b88-49f6-49a1-998e-3446cc742fa3",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# This is my firs parogram\n",
+    "# This parogram takes inputs from user\n",
+    "# it perforns basic calculations\n",
+    "# it lab task 1"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 35,
+   "id": "41595fef-5845-45d1-8c19-7146f95680f8",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'this is multiline commments this is practice parogram cover basic things, varriable and input'"
+      ]
+     },
+     "execution_count": 35,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\"\"\"this is multiline commments this is practice parogram cover basic things, varriable and input\"\"\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 36,
+   "id": "14c8204a-6169-4b23-9878-10873ca95e4a",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Comments example done\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Lab Task 1\n",
+    "# This program shows basic Python concepts\n",
+    "# It includes variables, input and calculations\n",
+    "\n",
+    "\"\"\"\n",
+    "This is a multi-line comment\n",
+    "The purpose of this program is to learn\n",
+    "basic programming in Python\n",
+    "\"\"\"\n",
+    "\n",
+    "print(\"Comments example done\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 48,
+   "id": "9506a367-32dc-4ae7-b0a7-c0fd75332b02",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "name = \" saad khan salar\"\n",
+    "Age = 22\n",
+    "hieght = 5.7\n",
+    "is_student =  True"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 49,
+   "id": "5a8f9e54-5184-4ce4-95ce-7001c21c9e0e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "<class 'str'>\n",
+      "<class 'str'>\n",
+      "<class 'float'>\n",
+      "<class 'bool'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(type(name))\n",
+    "print(type(age))\n",
+    "print(type(hieght))\n",
+    "print(type(is_student))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 50,
+   "id": "4e1b86b3-8fc8-452c-8649-b86f04960c2b",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "<class 'str'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "name = \"saad khan salar\"\n",
+    "print(type(name))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 51,
+   "id": "d455d795-81db-416a-99dc-6c72a3c7d4ed",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "<class 'int'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "num = 100\n",
+    "print(type(num))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 52,
+   "id": "8a82e2c6-92e9-4f57-92fc-686c7787c604",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "<class 'float'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "height = 5.7 \n",
+    "print(type(height))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 54,
+   "id": "b840786f-a09b-4f22-a4b5-b63cf94c1c8b",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "<class 'bool'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "is_student = True\n",
+    "print(type(is_student))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 57,
+   "id": "11e66dec-11e5-4d61-a5a8-35a2d48737a4",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your age: 22\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "After 10 years you will be: 32\n"
+     ]
+    }
+   ],
+   "source": [
+    "age =int(input(\"Enter your age:\"))\n",
+    "print(\"After 10 years you will be:\", age + 10)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 60,
+   "id": "a378b37d-a1ae-4298-b57c-ba091171b192",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your marks: 1 14\n",
+      "Enter your marks : 2  8\n",
+      "Enter your marks : 3  17\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "average marks: 27.666666666666668\n"
+     ]
+    }
+   ],
+   "source": [
+    "marks1 = float(input(\"Enter your marks: 1\"))\n",
+    "marks2 = float(input(\"Enter your marks : 2 \"))\n",
+    "marks3 = float(input(\"Enter your marks : 3 \"))\n",
+    "average = marks1 + marks2 + marks3 /3\n",
+    "print(\"average marks:\" ,  average)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "febdd3bd-b9d9-45df-b929-063c0a9370c9",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter a number:  24\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Even\n"
+     ]
+    }
+   ],
+   "source": [
+    "num = int(input(\"Enter a number: \"))\n",
+    "\n",
+    "if num % 2 == 0:\n",
+    "    print(\"Even\")\n",
+    "else:\n",
+    "    print(\"Odd\")\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "68a520cd-cb9f-4c9f-a814-e9f746ec4366",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter a number:  23\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Odd\n"
+     ]
+    }
+   ],
+   "source": [
+    "num = int(input(\"Enter a number: \"))\n",
+    "\n",
+    "if num % 2 == 0:\n",
+    "    print(\"Even\")\n",
+    "else:\n",
+    "    print(\"Odd\")\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "76109d2b-842c-4dcf-b779-24a77d4f309c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your marks:  84\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Grade: A\n"
+     ]
+    }
+   ],
+   "source": [
+    "marks = int(input(\"Enter your marks: \"))\n",
+    "\n",
+    "if marks >= 80:\n",
+    "    print(\"Grade: A\")\n",
+    "\n",
+    "elif marks >= 60:\n",
+    "    print(\"Grade: B\")\n",
+    "\n",
+    "elif marks >= 50:\n",
+    "    print(\"Grade: C\")\n",
+    "\n",
+    "else:\n",
+    "    print(\"Fail\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "4429b634-d0fb-4d34-8813-58ea9f3b4060",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your marks:  44\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Fail\n"
+     ]
+    }
+   ],
+   "source": [
+    "marks = int(input(\"Enter your marks: \"))\n",
+    "\n",
+    "if marks >= 80:\n",
+    "    print(\"Grade: A\")\n",
+    "\n",
+    "elif marks >= 60:\n",
+    "    print(\"Grade: B\")\n",
+    "\n",
+    "elif marks >= 50:\n",
+    "    print(\"Grade: C\")\n",
+    "\n",
+    "else:\n",
+    "    print(\"Fail\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "66329c4e-61d6-4801-a245-0a47fb92c6a1",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your age:  22\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "You can vote\n"
+     ]
+    }
+   ],
+   "source": [
+    "age = int(input(\"Enter your age: \"))\n",
+    "\n",
+    "if age >= 18:\n",
+    "    print(\"You can vote\")\n",
+    "else:\n",
+    "    print(\"Not eligible\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "6917a960-c0e2-43c5-b794-a10ddc358bdb",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter your age:  16\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Not eligible\n"
+     ]
+    }
+   ],
+   "source": [
+    "age = int(input(\"Enter your age: \"))\n",
+    "\n",
+    "if age >= 18:\n",
+    "    print(\"You can vote\")\n",
+    "else:\n",
+    "    print(\"Not eligible\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "701be526-dbe6-4b83-85da-0fa237c4a48c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "1\n",
+      "2\n",
+      "3\n",
+      "4\n",
+      "5\n",
+      "6\n",
+      "7\n",
+      "8\n",
+      "9\n",
+      "10\n"
+     ]
+    }
+   ],
+   "source": [
+    "for i in range(1, 11):\n",
+    "    print(i)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "faf0194c-f4ae-4305-a300-d366246b7714",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter a number:  5\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "5 x 1 = 5\n",
+      "5 x 2 = 10\n",
+      "5 x 3 = 15\n",
+      "5 x 4 = 20\n",
+      "5 x 5 = 25\n",
+      "5 x 6 = 30\n",
+      "5 x 7 = 35\n",
+      "5 x 8 = 40\n",
+      "5 x 9 = 45\n",
+      "5 x 10 = 50\n"
+     ]
+    }
+   ],
+   "source": [
+    "num = int(input(\"Enter a number: \"))\n",
+    "\n",
+    "for i in range(1, 11):\n",
+    "    print(num, \"x\", i, \"=\", num * i)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "48b81831-5735-4c39-a51f-ca9b6f6336c4",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Total: 55\n"
+     ]
+    }
+   ],
+   "source": [
+    "total = 0\n",
+    "\n",
+    "for i in range(1, 11):\n",
+    "    total = total + i\n",
+    "\n",
+    "print(\"Total:\", total)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "f0d5d2cc-882f-4d59-8e8a-fd852652cc68",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter password:  admin1234\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Correct Password!\n"
+     ]
+    }
+   ],
+   "source": [
+    "password = \"\"\n",
+    "\n",
+    "while password != \"admin1234\":\n",
+    "    password = input(\"Enter password: \")\n",
+    "\n",
+    "print(\"Correct Password!\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "d5c30f90-f552-485e-9d15-33149ffc3327",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter password:  admin123\n",
+      "Enter password:  admin1234\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Correct Password!\n"
+     ]
+    }
+   ],
+   "source": [
+    "password = \"\"\n",
+    "\n",
+    "while password != \"admin1234\":\n",
+    "    password = input(\"Enter password: \")\n",
+    "\n",
+    "print(\"Correct Password!\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "42475f18-aeaf-4be8-8dff-e07c996fd6f3",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "20.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "def average(a, b, c):\n",
+    "    return (a + b + c) / 3\n",
+    "\n",
+    "print(average(10, 20, 30))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1f75b9c3-cb97-48d5-bee7-16e02e589bc0",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.15"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
